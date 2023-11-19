0b413450c7828da0f44873fd52ca6b5e98abadd7
Nany033 <nany030303@outlook.com>
2023-11-19 11:47:40 -0600

    Ford-Fulkerson implementation

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 #include <fstream>
 #include <limits>
 #include <cmath>
+#include <string.h>
+#include <limits.h>
+#include <queue>
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
 
@@ -57,6 +60,51 @@ void nearestNeighbor(int **matrix,int N){
     cout<<"Total: "<<total<<endl;
 }
 //ford-fulkerson
+bool BFs(int rGraph[V][V], int s, int, t, int parent[]){
+    bool visited[V];
+    memset(visited, 0, sizeof(visited));
+    queue <int> q;
+    q.push(s);
+   visited[s] = true;
+   parent[s] = -1;
+   while (!q.empty()) {
+       int u = q.front();
+       q.pop();
+       for (int v = 0; v < V; v++) {
+           if (visited[v] == false && rGraph[u][v] > 0) {
+               q.push(v);
+               parent[v] = u;
+               visited[v] = true;
+           }
+       }
+   }
+   return (visited[t] == true);
+}
+
+int fordFulkerson(int graph[V][V], int s, int t) {
+   int u, v;
+   int rGraph[V][V];
+   for (u = 0; u < V; u++)
+       for (v = 0; v < V; v++)
+           rGraph[u][v] = graph[u][v];
+   int parent[V];
+   int max_flow = 0;
+   while (bfs(rGraph, s, t, parent)) {
+       int path_flow = INT_MAX;
+       for (v = t; v != s; v = parent[v]) {
+           u = parent[v];
+           path_flow = min(path_flow, rGraph[u][v]);
+       }
+       for (v = t; v != s; v = parent[v]) {
+           u = parent[v];
+           rGraph[u][v] -= path_flow;
+           rGraph[v][u] += path_flow;
+       }
+       max_flow += path_flow;
+   }
+   return max_flow;
+}
+
 
 int main(){
     ifstream in("input.cin");
